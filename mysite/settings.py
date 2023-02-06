ad28fbf34960a0bf70c42a6e1a6f7797b443720b
zeynebbesra <394777@ogr.ktu.edu.tr>
2023-02-06 14:59:40 +0300

    settings file modified

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 import os
 
-SECRET_KEY = os.getenv('SECRET')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
