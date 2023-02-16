1d7f83671f64b969ac6d5b8887de6aeeed7bfcb7
zeynebbesra <394777@ogr.ktu.edu.tr>
2023-02-16 16:31:00 +0300

    settings file updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 import os 
 SECRET_KEY = os.getenv('SECRET')
 
@@ -25,11 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['zbMovies.pythonanywhere.com']
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = ['zbMovies.pythonanywhere.com','127.0.0.1']
 
 
 
